e58dd08182079bc4373b1339c681d95a9c1bdd4c
duanpengyuan <duanpengyuan@qq.com>
2014-10-22 17:09:07 +0800

    2014.10.22 fix homework8 and add .gitignore

--- c/sbrk_test.c
+++ c/sbrk_test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <malloc.h>
 
 int main(){
 	size_t start, end;
@@ -8,14 +9,19 @@ int main(){
 	start = (size_t)sbrk(0);
 	printf("Our data segment at: %lu\n", start);
 
-	int* p = (int*)sbrk(sizeof(int));
-	*p = 1234;
-	printf("*p = %d\n", *p);
+	int* p = malloc(sizeof(int));
 
 	end = (size_t)sbrk(0);
 	printf("Our data segment at: %lu\n", end);
 
 	printf("We use mem of data: %lu\n", end - start);
+
+	
+	malloc_stats();
+
+	malloc(sizeof(char) * 1024 * 200); // 200M
+
+	malloc_stats();
 
 	return 0;
 }
--- homework/struct/homework8.c
+++ homework/struct/homework8.c
@@ -2,14 +2,23 @@
 
 #include "apue.h"
 
-unsigned int func(unsigned int n){
-	unsigned int i, sum = 1;
+// #define R
 
-	for(i = 1; i <= n; i++)
-		sum *= i;
+struct node_t {
+	unsigned int n;
+	struct node_t* next;
+};
+typedef struct node_t node_t;
 
-	return sum;
-}
+node_t* initStack();
+void push(node_t* head, unsigned int n);
+unsigned int pop(node_t* head);
+char is_empty(node_t* head);
+
+
+unsigned int func(unsigned int n);
+
+
 
 int main(int argc, char const *argv[])
 {
@@ -22,3 +31,93 @@ int main(int argc, char const *argv[])
 	
 	return 0;
 }
+
+node_t* initStack(){
+	node_t* head = NULL;
+
+	head = malloc(sizeof(node_t));
+	if(head == NULL){
+		exit(-1);
+	}
+
+	head->n = 0;
+	head->next = NULL;
+
+	return head;
+}
+
+void push(node_t* head, unsigned int n){
+	node_t* node = NULL;
+
+	node = malloc(sizeof(node_t));
+	if(node == NULL){
+		exit(-1);
+	}
+
+	node->n = n;
+
+	node->next = head->next;
+	head->next = node;
+}
+
+unsigned int pop(node_t* head){
+	if(is_empty(head)){	// stack empty
+		return 0;
+	}
+
+	node_t* node;
+	unsigned int n;
+
+	node = head->next;
+	head->next = node->next;
+	n = node->n;
+
+	if(node != NULL){
+		free(node);
+	}
+
+	return n;
+}
+
+
+char is_empty(node_t* head){
+	return head->next == NULL;
+}
+
+
+
+
+#ifdef R
+
+unsigned int func(unsigned int n){
+
+	if(n == 0)
+		return 1;
+	else
+		return n * func(n - 1);
+}
+
+#else
+
+unsigned int func(unsigned int n){
+	node_t* stack = initStack();
+
+	if(n == 0)
+		return 1;
+
+	while(n > 0){
+		push(stack, n);
+
+		n--;
+	}
+
+	unsigned int sum = 1;
+
+	while(!is_empty(stack)){
+		sum *= pop(stack);
+	}
+
+	return sum;
+}
+
+#endif
